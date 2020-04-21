86ed55c0d47110fd5c083b09b103db73fecdf605
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2020-04-21 22:26:45 +0700

    Add google analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-142494062-2");
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 const CoronaContainer = React.lazy(() => import("./pages/corona"));
 
 export default function App() {
